1a755294a3c6f61f4f3233c8c84c1a6cb06d2453
Emma Kinch <eokinch@gmail.com>
2017-12-19 18:00:45 -0500

    inital upload

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,3 +1,49 @@
-const ohHey = "Hello World";
+const coffeeApp = {};
 
-console.log(ohHey);
+coffeeApp.key = '4bba844de5c34fdd2ce7dfe6963d7f05';
+coffeeApp.id = '64cb1661';
+coffeeApp.withAlochol = '';
+
+coffeeApp.formSubmit = () => {
+	$('#selection-form').submit( (e) => {
+		e.preventDefault();
+		const withAlochol = $('input[name=selection2]:checked').val();
+		if (withAlochol === 'yes') {
+			coffeeApp.withAlochol = 'allowedIngredient[]=wine';
+		} else {
+			coffeeApp.withAlochol = 'excludedIngredient[]=wine&excludedIngredient[]=kahlúa';
+		}
+		if(withAlochol === undefined) {
+			$('.selection-form_error').css('visibility', 'visible')
+		}else{
+			coffeeApp.getData();
+		}
+	})
+}
+
+coffeeApp.getData = () => {
+	$.ajax({
+		url: `http://api.yummly.com/v1/api/recipes?_app_id=${coffeeApp.id}&_app_key=${coffeeApp.key}&q=coffee&allowedCourse[]=course^course-Beverages&${coffeeApp.withAlochol}&maxResult=100`,
+		method: 'GET',
+		dataType: 'JSON'
+	}).then((data) => {
+		coffeeApp.reduceData(data.matches)
+	})
+}
+
+coffeeApp.reduceData = (data) => {
+	const getThreeRecipies = _.sample(data, 3);
+
+	coffeeApp.displayData(getThreeRecipies)
+}
+
+coffeeApp.displayData = (data) => {
+	data.forEach((e) => {
+		console.log(e)
+	})
+	// $('.drink-results_displayed')
+}
+
+$(() => {
+	coffeeApp.formSubmit();
+})
